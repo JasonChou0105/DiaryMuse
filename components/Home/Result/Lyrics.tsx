8b52bd16074cf3caa35fc2d0d09083c8ab03ca64
Jason Chou <JasonChou0105@gmail.com>
2025-01-18 23:48:18 -0500

    home page form styles

--- components/Home/Result/Lyrics.tsx
+++ components/Home/Result/Lyrics.tsx
@@ -1,13 +1,9 @@
 function Lyrics() {
   return (
-    <div className="p-6 bg-beige-200 rounded-md shadow-md w-2/3 text-xl mt-8">
+    <div className="p-6 bg-beige-200 rounded-md shadow-md w-full h-full text-lg mt-8">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem inventore,
       odit atque reprehenderit quo esse ut optio, quibusdam deserunt excepturi
-      nihil, aspernatur culpa aliquam! Temporibus perferendis alias nesciunt eos
-      veritatis?Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Necessitatibus quo repellendus itaque provident dolor, vero eos ab rem
-      deleniti perspiciatis corporis distinctio incidunt molestiae temporibus
-      adipisci quos iure magnam ad!Loremlor
+      nihil, aspernatur culpa aliquam!
     </div>
   );
 }
